929db4e59d37df6b7c1772a433e0f249daf59805
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2023-05-26 21:39:46 -0300

    update de novas atualizações do projeto

---
+++ src/Bibliotecas/SwiperCardEffects.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper', {
+    spaceBetween: 20,
+    slidesPerView: 2,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
